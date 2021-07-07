cdb248376265b6c357351fc1c4fdb95626c38dc6
Dan <kofsitho@twosun.com>
2021-07-07 09:48:26 +0900

    update

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  await app.listen(3000, '0.0.0.0');
+  const port = 22001; // 3000
+
+  await app.listen(port, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 
